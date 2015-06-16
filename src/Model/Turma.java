2ac5f774dda448cf136e42a8ee017a5a0fc57d52
info206 <info206@LAB02-06.edupit.intra>
2015-06-16 14:55:48 -0300

    new view

---
+++ src/Model/Turma.java
@@ -0,0 +1,18 @@
+package Model;
+
+public class Turma {
+    String idTurma;
+
+    public Turma(String idTurma) {
+        this.idTurma = idTurma;
+    }
+
+    public String getIdTurma() {
+        return idTurma;
+    }
+
+    public void setIdTurma(String idTurma) {
+        this.idTurma = idTurma;
+    }
+    
+}
